b6965407353733a1eed1c8a76d63fc482ef6d50f
Tom Palmer <remlapmot@hotmail.com>
2022-01-17 18:33:44 +0000

    Skip tests on CRAN

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,15 +1,18 @@
 test_that("Test get_bcftools_binary()", {
+  skip_on_cran()
   skip_on_os(os = "windows")
   bcftools_exe <- get_bcftools_binary()
   expect_true(file.exists(bcftools_exe))
 })
 
 test_that("Test get_plink_binary()", {
+  skip_on_cran()
   plink_exe <- get_plink_binary()
   expect_true(file.exists(plink_exe))
 })
 
 test_that("Test get_gcta_binary()", {
+  skip_on_cran()
   gcta_exe <- get_gcta_binary()
   expect_true(file.exists(gcta_exe))
 })
